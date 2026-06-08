66d046c6f05faca93769632fe0c7128d2ebda888
Yash Pandit <yashpandit1715@gmail.com>
2026-06-08 23:26:25 +0530

    deleted

--- agriculture_service_center/includes/footer.php
+++
@@ -1,8 +0,0 @@
-
-</style>
-
-<footer class="site-footer">
-    <p>© 2026 Agriculture Service Center</p>
-</footer>
-
-
--- agriculture_service_center/user/verify_payment.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../config/db_connect.php";
-
-$booking_id = $_GET['booking_id'];
-
-pg_query($conn, "UPDATE bookings SET status='pending' WHERE booking_id=$booking_id");
-
-header("Location: payment_success.php?booking_id=$booking_id");
-exit;
